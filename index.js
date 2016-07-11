ed3cebb6ebbb9469331ba7ba8e7aae7a829faca1
Marijn Haverbeke <marijn@haverbeke.nl>
2016-07-11 14:23:20 +0200

    Make the set of fields in the editor state extendable

--- index.js
+++ index.js
@@ -7,6 +7,106 @@ const {Remapping} = require("../transform")
 const {EditorTransform} = require("./transform")
 exports.EditorTransform = EditorTransform
 
+function currentMarks(doc, selection) {
+  return selection.head == null ? Mark.none : doc.marksAt(selection.head)
+}
+
+const nullOptions = {}
+
+function hasProp(obj, prop) {
+  return Object.prototype.hasOwnProperty.call(obj, prop)
+}
+
+function editorStateClass(fields) {
+  const fieldNames = Object.keys(fields)
+
+  class EditorState {
+    constructor(doc, selection, storedMarks, updated, preserved) {
+      this.doc = doc
+      this.selection = selection
+      this.storedMarks = storedMarks
+      for (let i = 0; i < fieldNames.length; i++) {
+        let name = fieldNames[i]
+        this[name] = updated && hasProp(updated, name) ? updated[name] : preserved[name]
+      }
+    }
+
+    // :: Schema
+    get schema() {
+      return this.doc.type.schema
+    }
+
+    applyTransform(transform, options = nullOptions) {
+      if (!transform.docs[0].eq(this.doc))
+        throw new RangeError("Applying a transform that does not start with the current document")
+      let updated = {}
+      for (let i = 0; i < fieldNames.length; i++) {
+        let name = fieldNames[i], val = this[name]
+        if (val && val.applyTransform)
+          updated[name] = val.applyTransform(transform, options)
+      }
+      return new EditorState(transform.doc,
+                             options.selection || this.selection.map(transform.doc, transform.mapping),
+                             options.selection ? null : this.storedMarks,
+                             updated, this)
+    }
+
+    applySelection(selection, options = nullOptions) {
+      let updated = {}
+      for (let i = 0; i < fieldNames.length; i++) {
+        let name = fieldNames[i], val = this[name]
+        if (val && val.applySelection)
+          updated[name] = val.applySelection(selection, options)
+      }
+      return new EditorState(this.doc, selection, null, updated, this)
+    }
+
+    addActiveMark(mark) {
+      if (!this.selection.empty) return this
+      return new EditorState(this.doc, this.selection,
+                             mark.addToSet(this.storedMarks || currentMarks(this.doc, this.selection)),
+                             null, this)
+    }
+
+    removeActiveMark(markType) {
+      if (!this.selection.empty) return this
+      return new EditorState(this.doc, this.selection,
+                             markType.removeFromSet(this.storedMarks || currentMarks(this.doc, this.selection)),
+                             null, this)
+    }
+
+    // :: EditorTransform
+    // Create a selection-aware `Transform` object.
+    get tr() { return new EditorTransform(this) }
+
+    update(fields) {
+      return new EditorState(this.doc || this.doc,
+                             this.selection || this.selection,
+                             this.storedMarks || this.storedMarks,
+                             fields, this)
+    }
+
+    static fromDoc(doc, selection) {
+      if (!selection) selection = Selection.atStart(doc)
+      let initial = {}
+      fieldNames.forEach(name => initial[name] = fields[name](doc, selection))
+      return new EditorState(doc, selection, null, initial)
+    }
+
+    static fromSchema(schema) {
+      return this.fromDoc(schema.nodes.doc.createAndFill())
+    }
+
+    static extend(addFields) {
+      let newFields = {}
+      fieldNames.forEach(name => newFields[name] = fields[name])
+      for (let name in addFields) if (hasProp(addFields, name)) newFields[name] = addFields[name]
+      return editorStateClass(newFields)
+    }
+  }
+  return EditorState
+}
+
 class ViewState {
   constructor(inDOMChange, domChangeMapping, scrollToSelection) {
     this.inDOMChange = inDOMChange
@@ -35,69 +135,4 @@ class ViewState {
 ViewState.initial = new ViewState(null, null, false)
 exports.ViewState = ViewState
 
-function currentMarks(doc, selection) {
-  return selection.head == null ? Mark.none : doc.marksAt(selection.head)
-}
-
-const nullOptions = {}
-
-class EditorState {
-  constructor(doc, selection, storedMarks, view) {
-    this.doc = doc
-    this.selection = selection
-    this.storedMarks = storedMarks
-    this.view = view
-  }
-
-  // :: Schema
-  get schema() {
-    return this.doc.type.schema
-  }
-
-  applyTransform(transform, options = nullOptions) {
-    if (!transform.docs[0].eq(this.doc))
-      throw new RangeError("Applying a transform that does not start with the current document")
-    return new EditorState(transform.doc,
-                           options.selection || this.selection.map(transform.doc, transform.mapping),
-                           options.selection ? null : this.storedMarks,
-                           this.view.applyTransform(transform, options))
-  }
-
-  applySelection(selection, options = nullOptions) {
-    return new EditorState(this.doc, selection, null, this.view.applySelection(selection, options))
-  }
-
-  addActiveMark(mark) {
-    if (!this.selection.empty) return this
-    return new EditorState(this.doc, this.selection,
-                           mark.addToSet(this.storedMarks || currentMarks(this.doc, this.selection)),
-                           this.view)
-  }
-
-  removeActiveMark(markType) {
-    if (!this.selection.empty) return this
-    return new EditorState(this.doc, this.selection,
-                           markType.removeFromSet(this.storedMarks || currentMarks(this.doc, this.selection)),
-                           this.view)
-  }
-
-  // :: EditorTransform
-  // Create a selection-aware `Transform` object.
-  get tr() { return new EditorTransform(this) }
-
-  update(fields) {
-    return new EditorState(fields.doc || this.doc,
-                           fields.selection || this.selection,
-                           fields.storedMarks || this.storedMarks,
-                           fields.view || this.view)
-  }
-
-  static fromDoc(doc, selection) {
-    return new EditorState(doc, selection || Selection.atStart(doc), null, ViewState.initial)
-  }
-
-  static fromSchema(schema) {
-    return this.fromDoc(schema.nodes.doc.createAndFill())
-  }
-}
-exports.EditorState = EditorState
+exports.EditorState = editorStateClass({view: () => ViewState.initial})
